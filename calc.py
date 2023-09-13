352bcd27fd9fea2f5ba8092491c94d892451e0ee
Abhinaya <144606589+Abhinayasamala@users.noreply.github.com>
2023-09-13 22:40:01 +0530

    calc.py

--- calc.py
+++ calc.py
@@ -1,14 +1,10 @@
 #python program for calculator
-#function to addition of two numbers
 def add (n1,n2):
     return n1 + n2
-#function to subtraction of two numbers
 def sub(n1,n2):
     return n1 - n2
-#function to multiplication of two numbers
 def mul(n1,n2):
     return n1 * n2
-#function to division of two numbers
 def div(n1,n2):
    return n1 / n2
 def perform_calculation(n1, n2, operation):
@@ -27,16 +23,15 @@ def perform_calculation(n1, n2, operation):
     else:
         return "Invalid operation"
 
-# Prompt the user for input
 try:
     n1 = float(input("Enter the first number: "))
     n2 = float(input("Enter the second number: "))
     operation = input("Choose an operation (+, -, *, /): ")
-    # Perform the calculation
     result = perform_calculation(n1, n2, operation)
-    # Display the result
     print("Result:",result)
 except :
     print("Invalid input. Please enter valid numbers.")
+
+
 
 
